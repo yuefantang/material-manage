d80e1b3b3846782a19d53a05a9e3dff4a1ed052b
yuefantang <1603099869@qq.com>
2018-12-27 11:38:57 +0800

    细节修改

--- material-web/src/main/java/com/dongyu/company/MaterialWebApplication.java
+++ material-web/src/main/java/com/dongyu/company/MaterialWebApplication.java
@@ -1,13 +1,17 @@
 package com.dongyu.company;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -16,5 +20,10 @@ import java.io.File;
 public class MaterialWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(MaterialWebApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ApplicationContext context){
+       return  args -> Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
