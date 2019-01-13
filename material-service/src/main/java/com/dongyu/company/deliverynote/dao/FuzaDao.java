6ba827e1e0b2e8486338b281464db561c54d4029
yuefantang <1603099869@qq.com>
2019-01-13 23:02:21 +0800

    自定义sql查询语句返回结果用bean接收

--- material-service/src/main/java/com/dongyu/company/deliverynote/dao/FuzaDao.java
+++ material-service/src/main/java/com/dongyu/company/deliverynote/dao/FuzaDao.java
@@ -1,11 +1,15 @@
 package com.dongyu.company.deliverynote.dao;
 
+import com.dongyu.company.deliverynote.dto.DeliveryQueryDTO;
+import com.dongyu.company.deliverynote.dto.Fuz;
+import org.hibernate.Query;
+import org.hibernate.SQLQuery;
+import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import java.util.List;
 
 /**
  * 财务对账查询
@@ -19,16 +23,16 @@ public class FuzaDao {
     @Autowired
     private EntityManager entityManager;
 
-    public void  query(){
-        StringBuilder sql=new StringBuilder();
-
-        sql.append("");
-
-        //查询结果
-        Query query = entityManager.createNativeQuery(sql.toString());
-        //查询总数
-
-
+    public List<Fuz> query(DeliveryQueryDTO deliveryQueryDTO) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT COUNT(*)AS total,entity_id as id FROM t_operation_record  GROUP BY entity,entity_id");
+        SQLQuery sqlQuery = entityManager.createNativeQuery(sql.toString()).unwrap(SQLQuery.class);
+        Query query =sqlQuery.setResultTransformer(Transformers.aliasToBean(Fuz.class));
+        query.setFirstResult(deliveryQueryDTO.getPageNo() * deliveryQueryDTO.getPageSize());
+        query.setMaxResults(deliveryQueryDTO.getPageSize());
+        List<Fuz> resultList = query.list();
+        entityManager.close();
+        return resultList;
     }
 
 }
---
+++ material-service/src/main/java/com/dongyu/company/deliverynote/dto/Fuz.java
@@ -0,0 +1,20 @@
+package com.dongyu.company.deliverynote.dto;
+
+import lombok.Data;
+
+import java.math.BigInteger;
+
+/**
+ * TODO:请添加描述
+ *
+ * @author TYF
+ * @date 2019/1/13
+ * @since 1.0.0
+ */
+@Data
+public class Fuz {
+
+    private BigInteger total;
+
+    private  BigInteger id;
+}
